909465a644f7d72d945031abfd4142bc00d56b3d
manucirne <manucirne@gmail.com>
2021-09-29 16:31:21 -0300

    traduçoes

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 from dev_aberto import hello
+from babel.dates import format_datetime
+from datetime import datetime
+import gettext
+
+gettext.install('hello', localedir='locale')
 
 if __name__ == '__main__':
     date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+    date = format_datetime(datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ'))
+    print(_('Ultimo commit feito em: '), date, _(' por '), name)
